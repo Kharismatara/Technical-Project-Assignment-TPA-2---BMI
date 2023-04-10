c79085bd16ad289a6291dc1f0bd94f162f2cc8ee
kharisma tara mukti <kharismataram@gmail.com>
2023-04-10 22:15:41 +0700

    update terbaru dengan metod poe

--- js/script.js
+++ js/script.js
@@ -1,28 +1,29 @@
 function calculateBMI(event) {
-    event.preventDefault();
-    //mendapatkan nilai input dari form
-    let weight = parseFloat(document.getElementById("weight").value);
-    let height = parseFloat(document.getElementById("height").value);
+  event.preventDefault();
+  //mendapatkan nilai input dari form
+  let weight = parseFloat(document.getElementById("weight").value);
+  let height = parseFloat(document.getElementById("height").value);
 
-    if (isNaN(weight) || isNaN(height) || height <= 0 || weight <= 0) {
-        alert("The value entered cannot be 0 or minus");
-        return;
-    }
-    let bmi = weight / Math.pow(height / 100, 2);
+  if (isNaN(weight) || isNaN(height) || height <= 0 || weight <= 0) {
+    alert("The value entered cannot be 0 or minus");
+    return;
+  }
+  let bmi = weight / Math.pow(height / 100, 2);
 
-    document.getElementById("bmi").textContent = bmi.toFixed(1);
+  //untuk menampilkan nilai 2.123 = 21.1
+  document.getElementById("bmi").textContent = bmi.toFixed(1);
 
-    let bmiCategory = "";
-    if (bmi < 18.5) {
-        bmiCategory = "Underweight";
-    } else if (bmi < 25) {
-        bmiCategory = "Normal Weight";
-    } else if (bmi < 30) {
-        bmiCategory = "Overweight";
-    } else {
-        bmiCategory = "Obesity";
-    }
+  let bmiCategory = "";
+  if (bmi < 18.5) {
+    bmiCategory = "Underweight";
+  } else if (bmi < 25) {
+    bmiCategory = "Normal Weight";
+  } else if (bmi < 30) {
+    bmiCategory = "Overweight";
+  } else {
+    bmiCategory = "Obesity";
+  }
 
-    document.getElementById("bmiCategory").textContent = bmiCategory;
-    document.getElementById("result").style.display = "block";
+  document.getElementById("bmiCategory").textContent = bmiCategory;
+  document.getElementById("result").style.display = "block";
 }
